278339a0481915e9ed42ef52a84109506a56734d
Kaylee Yin <kayleeyin2@gmail.com>
2020-07-29 21:22:53 -0400

    day 1!

---
+++ iAct/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iAct
+//
+//  Created by Kaylee Yin on 7/29/20.
+//  Copyright © 2020 Kaylee Yin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iAct/YemenDescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YemenDescriptionViewController.swift
+//  iAct
+//
+//  Created by Kaylee Yin on 7/29/20.
+//  Copyright © 2020 Kaylee Yin. All rights reserved.
+//
+
+import UIKit
+
+class YemenDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
